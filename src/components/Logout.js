8316df953c7a811701a327247b15b12e6c23b565
Astrospence <illustrator.wood@gmail.com>
2021-12-17 10:58:45 -0700

    completed Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
+const Logout = () => {      
+    const { push } = useHistory(); 
+    
+    useEffect(() => {
+        axiosWithAuth()
+            .post("/logout")
+            .then(() => {
+                localStorage.removeItem("token")
+                localStorage.removeItem("role")
+                localStorage.removeItem("username")
+                push("/")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+    
     return(<div></div>);
 }
 
